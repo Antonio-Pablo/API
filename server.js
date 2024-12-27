c97edf7d7811806700630b5db1202a9b44983675
Antonio Pablo <pabloisaias2020@gmail.com>
2024-12-27 15:44:25 -0300

    Atualizar Usuario Ok

--- server.js
+++ server.js
@@ -24,6 +24,21 @@ app.get("/users", async (req, res) => {
   const users = await prisma.user.findMany();
   res.status(200).json(users);
 });
+
+app.put("/users/:id", async (req, res) => {
+  await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+  });
+  res.status(201).json(req.body);
+});
+
 app.listen(3000);
 
 /*
