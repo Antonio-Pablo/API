64fd44d326c6e11848b0321a2b967f345ee3db7e
Antonio Pablo <pabloisaias2020@gmail.com>
2024-12-27 15:26:46 -0300

    Criação de Usuário Ok

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 import express, { json } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = express();
 const users = [];
 app.use(express.json());
+
 //rota de criação de usuario
-app.post("/users", (req, res) => {
-  users.push(req.body);
-  res.status(201).json("Ok Post certo");
+app.post("/users", async (req, res) => {
+  await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+  });
+  res.status(201).json(req.body);
 });
+
 //rotas de listagem de usuarios
 app.get("/users", (req, res) => {
   res.status(200).json(req.body);
 });
 app.listen(3000);
-
 
 /*
     Cria nossa API de usuarios
